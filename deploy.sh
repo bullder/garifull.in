e85020a16691f6739b32098c41f28bd34a95dc47
Michael Garifullin <michael.garifullin@smartbox.com>
2019-04-15 14:58:46 +0100

    change deploy

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,12 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 hugo -t hugo-coder
-
 cd public
 git add .
-
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
-
-git push origin master -f
+git push origin master
 cd ..
