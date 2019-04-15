3c16867e6a5626a99764a5c20a682a0e426b141f
Michael Garifullin <michael.garifullin@smartbox.com>
2019-04-15 16:53:05 +0100

    ammend deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,29 @@
-#!/bin/bash
+#!/bin/sh
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
-hugo -t hugo-coder
-cd public
-git add .
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
+DIR=$(dirname "$0")
+
+cd $DIR/..
+
+if [[ $(git status -s) ]]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
 fi
-git commit -m "$msg"
-git push origin master
-cd ..
+
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+echo "Checking out gh-pages branch into public"
+git worktree add -B gh-pages public upstream/gh-pages
+
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Generating site"
+hugo
+
+echo "Updating gh-pages branch"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
