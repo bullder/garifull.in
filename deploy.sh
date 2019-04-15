b3d9fd4c92db9552c45e50335236a946cfb141c0
Michael Garifullin <michael.garifullin@smartbox.com>
2019-04-15 17:29:07 +0100

    as

--- deploy.sh
+++ deploy.sh
@@ -13,17 +13,17 @@ fi
 echo "Deleting old publication"
 rm -rf public
 mkdir public
-# git worktree prune
-# rm -rf .git/worktrees/public/
+git worktree prune
+rm -rf .git/worktrees/public/
 
-# echo "Checking out gh-pages branch into public"
-# git worktree add -B gh-pages public upstream/gh-pages
+echo "Checking out gh-pages branch into public"
+git worktree add -B gh-pages public upstream/gh-pages
 
 echo "Removing existing files"
-# rm -rf public/*
+rm -rf public/*
 
 echo "Generating site"
 hugo -t hugo-coder
 
 echo "Updating gh-pages branch"
-# cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
