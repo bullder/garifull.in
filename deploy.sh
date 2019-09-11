8cbe8bcfbe4db4d499f53a04c3ad119df7883de1
Michael Garifullin <michael.garifullin@smartbox.com>
2019-09-11 14:01:46 +0100

    cv

--- deploy.sh
+++ deploy.sh
@@ -25,5 +25,7 @@ rm -rf public/*
 echo "Generating site"
 hugo -t hugo-coder
 
+cp static/* public/
+
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
